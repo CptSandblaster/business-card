1493019ee8d28410c0c1a2d0cf9980e88919af26
Sebastian Alteryd <sebastian.alteryd@gmail.com>
2023-02-03 17:52:13 +0100

    Initial setup of business card repo

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    'src/*.html'
+    ],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
