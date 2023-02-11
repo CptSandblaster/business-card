52cc819ea95ff98a72fd84995b4e061366ec725a
Sebastian Alteryd <sebastian.alteryd@gmail.com>
2023-02-11 16:01:12 +0100

    new design

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,21 @@ module.exports = {
     ],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      'sans': 'Inconsolata',
+
+    },
     extend: {
       colors: {
+        'primary': '#2F4550',
+        'text': '#5CC8FF',
+        'background': '#031927',
+        'light': '#5CC8FF',
+        'accent': '#AF9164'
       },
+      fontFamily: {
+        'title': 'Pacifico'
+      }
     },
   },
   variants: {
